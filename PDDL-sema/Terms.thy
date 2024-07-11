04b92733486eadd22744891193624ba1cd38e5e1
David Wang <72378768+david-wang-0@users.noreply.github.com>
2024-07-11 15:19:38 +0100

    Shorter representation

--- PDDL-sema/Terms.thy
+++ PDDL-sema/Terms.thy
@@ -11,20 +11,18 @@ begin
   (* Normal forms for terms and interpretations *)
   inductive nf_fi::"'a function_interpretation \<Rightarrow> bool" 
         and nf_term::"'a function_interpretation \<Rightarrow> 'a term \<Rightarrow> bool" where
-    "\<lbrakk>\<not>(\<exists>obj. Sym obj \<in> dom ofi); 
-        \<forall>(f, a) \<in> Map.graph ofi. 
-          (\<forall>t \<in> (subterms f - {f}). nf_term ofi t) 
-        \<and> (\<forall>t \<in> subterms a. nf_term ofi a)\<rbrakk> 
-      \<Longrightarrow> nf_fi ofi"
-  | "nf_term ofi (Sym _)"
-  | "\<lbrakk>nf_fi ofi; list_all (nf_term ofi) as; ofi (Fun f as) = None\<rbrakk> 
-      \<Longrightarrow> nf_term ofi (Fun f as)"
+    "\<lbrakk>\<not>(\<exists>obj. Sym obj \<in> dom fi); 
+        \<forall>(f, a) \<in> Map.graph fi. (\<forall>t \<in> subterms t - {t}. nf_term fi t)  \<and> nf_term fi a\<rbrakk> 
+      \<Longrightarrow> nf_fi fi"
+  | "nf_term fi (Sym _)"
+  | "\<lbrakk>nf_fi fi; list_all (nf_term fi) as; fi (Fun f as) = None\<rbrakk> 
+      \<Longrightarrow> nf_term fi (Fun f as)"
   
   locale term_eq =
-    fixes ofi::"object function_interpretation"
-    assumes "nf_ofi ofi"
+    fixes fi::"'sym function_interpretation"
+    assumes "nf_fi fi"
   begin
-    definition "eq \<equiv> equivclp (\<lambda>x y. ofi x = Some y)"
+    definition "eq \<equiv> equivclp (\<lambda>x y. fi x = Some y)"
   end
 
 end
