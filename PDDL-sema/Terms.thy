9b76ea7ae6f406217c5119161654e899266291f7
David Wang <72378768+david-wang-0@users.noreply.github.com>
2024-07-15 17:10:57 +0100

    WIP: equality by reduction

--- PDDL-sema/Terms.thy
+++ PDDL-sema/Terms.thy
@@ -2,22 +2,54 @@ theory Terms
   imports AST1
 begin
 
-  (* this is an equivalence relation *)
   type_synonym 'sym function_interpretation = "'sym term \<rightharpoonup> 'sym term"
 
   fun subterms::"'a term \<Rightarrow> 'a term set" where
     "subterms (Sym x) = {Sym x}"
   | "subterms (Fun f as) = insert (Fun f as) (\<Union> (subterms ` (set as)))"
 
+  lemma term_in_subterms: "x \<in> subterms x"
+    by (cases x) auto
+  
+  lemma args_in_subterms: "set as \<subseteq> subterms (Fun f as)"
+    using term_in_subterms by auto
+    
   inductive nf_fi::"'a function_interpretation \<Rightarrow> bool"
         and nf_term::"'a function_interpretation \<Rightarrow> 'a term \<Rightarrow> bool" where
     "\<lbrakk>\<not>(\<exists>obj. Sym obj \<in> dom fi); 
-        \<forall>(l, r) \<in> Map.graph fi. (\<forall>t \<in> subterms l - {l}. nf_term fi t)  \<and> nf_term fi r\<rbrakk> 
+        \<forall>(l, r) \<in> Map.graph fi. \<exists>f as. Fun f as = l \<and> list_all (nf_term fi) as \<and> nf_term fi r\<rbrakk> 
       \<Longrightarrow> nf_fi fi"
-  | "nf_term fi (Sym _)"
+  | "nf_fi fi \<Longrightarrow> nf_term fi (Sym s)"
   | "\<lbrakk>nf_fi fi; list_all (nf_term fi) as; fi (Fun f as) = None\<rbrakk> 
       \<Longrightarrow> nf_term fi (Fun f as)"
 
+lemma nf_subterms:
+  "nf_term fi x \<longleftrightarrow> (\<forall>t \<in> subterms x. nf_term fi t)"
+proof (induction x)
+  case (Sym x)
+  then show ?case by auto
+next
+  case (Fun f as)
+  show ?case 
+  proof (rule iffI)
+    assume a: "nf_term fi (Fun f as)"
+    hence "list_all (nf_term fi) as"
+      apply (cases rule: nf_term.cases)
+      by auto
+    from this[simplified list_all_iff]
+    have "\<forall>a \<in> set as. \<forall>t \<in> subterms a. nf_term fi t" 
+      using Fun.IH by blast
+    hence "\<forall>t \<in> \<Union> (subterms ` (set as)). nf_term fi t" by simp
+    with a
+    show "\<forall>t\<in>subterms (Fun f as). nf_term fi t" by simp
+  next
+    assume "\<forall>t\<in>subterms (Fun f as). nf_term fi t"
+    thus "nf_term fi (Fun f as)" by simp
+  qed
+qed
+
+  (* Inspired by the proof of the Church-Rosser Theorem by Per-Martin Löf, as presented in the lecture
+    notes by M. Sørensen and P. Urzyczyn. *) 
   locale term_eq =
     fixes fi::"'sym function_interpretation"
   begin
@@ -61,7 +93,7 @@ begin
   
   locale decidable_eq = term_eq fi 
     for fi::"'sym term \<Rightarrow> 'sym term option" +
-    assumes "nf_fi fi"
+    assumes nf: "nf_fi fi"
   begin
     fun normalise_term::"'sym term \<Rightarrow> 'sym term" where
       "normalise_term (Sym s) = (Sym s)"
@@ -76,17 +108,228 @@ begin
   definition decidable_eq::"'sym term \<Rightarrow> 'sym term \<Rightarrow> bool" where
     "decidable_eq a b \<equiv> (normalise_term a) = (normalise_term b)"
 
+lemma nf_cannot_reduce': 
+  assumes "nf_term fi t"
+  shows "t \<notin> dom fi"
+proof (cases t)
+  case (Sym s)
+  with nf
+  show "t \<notin> dom fi" 
+    apply (cases rule: nf_fi.cases)
+    by simp
+next
+  case (Fun f as)
+  with assms(1)
+  show ?thesis 
+    apply (cases rule: nf_term.cases)
+    by auto
+qed
+
+lemma nf_cannot_reduce: 
+  assumes "nf_term fi t"
+      and "t \<rightarrow>\<^sub>t t'"
+    shows "t = t'"
+  using assms(2, 1)
+proof (induction rule: reduce.induct)
+  case (refl t)
+  then show ?case by simp
+next
+  case (trans t1 t2 t3)
+  then show ?case by simp
+next
+  case (def t1 t2)
+  from nf_cannot_reduce'[OF \<open>nf_term fi t1\<close>] Map.graph_domD[OF \<open>(t1, t2) \<in> Map.graph fi\<close>]
+  have "False" by simp
+  then show ?case ..
+next
+  case (app as as' f)
+  from \<open>nf_term fi (Fun f as)\<close>
+  have "list_all (nf_term fi) as" 
+    apply (cases rule: nf_term.cases)
+    by simp
+  with \<open>list_all2 (\<lambda>x1 x2. x1 \<rightarrow>\<^sub>t x2 \<and> (nf_term fi x1 \<longrightarrow> x1 = x2)) as as'\<close>
+  have "as = as'" 
+    apply (induction rule: list_all2_induct)
+    by auto
+  then show ?case by simp
+qed
+
+lemma unique_reduction:
+  assumes "t1 \<rightarrow>\<^sub>t t2"
+      and "(t1, t2) \<in> Map.graph fi"
+      and "t1 \<rightarrow>\<^sub>t t'"
+    shows "t' = t1 \<or> t' = t2"
+proof (cases t1)
+  case (Sym x)
+  with assms(2)
+  have "False" 
+    apply (cases rule: nf_fi.cases[OF nf])
+    apply (drule graph_domD)
+    by simp
+  then show ?thesis by simp
+next
+  case (Fun f as)
+  show ?thesis
+  proof (rule ccontr)
+    assume "\<not> (t' = t1 \<or> t' = t2)"
+    hence "t' \<noteq> t1" "t' \<noteq> t2" by auto
+    from assms(3, 2) this
+    have "\<exists>as'. list_all2 (\<rightarrow>\<^sub>t) as as' \<and> as \<noteq> as'"
+      unfolding Fun
+    proof (induction "Fun f as" t' arbitrary: t2 rule: reduce.induct)
+      case (refl t)
+      then show ?case by simp
+    next
+      case (trans t t' t'')
+      show ?case 
+      proof (cases "t = Fun f as")
+        case True
+        with trans
+        show ?thesis by simp
+      next
+        case False
+        from \<open>(Fun f as, t'') \<in> Map.graph fi\<close>
+        have "nf_term fi t''" 
+          by (cases rule: nf_fi.cases[OF nf]) auto 
+        have "t \<noteq> t''" 
+        proof
+          assume "t = t''"
+          from nf_cannot_reduce[OF \<open>nf_term fi t''\<close> trans(3)[simplified this]] trans(7)
+          show False by simp
+        qed    
+        from trans(2)[OF trans(5) False this]
+        show ?thesis .
+      qed
+    next
+      case (def t t2')
+      from in_graphD[OF this(1)] in_graphD[OF this(2)] this(4)
+      show ?case by simp
+    next
+      case (app as' t')
+      from this (1)
+      have "list_all2 (\<rightarrow>\<^sub>t) as as'" 
+        by (induction rule: list_all2_induct) auto
+      with app(3)
+      show ?case by blast
+    qed
+    then obtain as' where
+      as': "list_all2 (\<rightarrow>\<^sub>t) as as'"
+      "as \<noteq> as'" by auto 
+    
+    from assms(2)[simplified Fun]
+    have "list_all (nf_term fi) as" 
+    proof (cases rule: nf_fi.cases[OF nf])
+      case (1 fi)
+      with assms(2)[simplified Fun]
+      show ?thesis by fast 
+    qed
+    with as'(1)
+    have "as = as'" 
+    proof (subst list_all2_eq, induction rule: list_all2_induct)
+      case Nil
+      then show ?case by auto
+    next
+      case (Cons x y xs ys)
+      hence "nf_term fi x" by auto
+      with Cons(1)
+      have "x = y" using nf_cannot_reduce by simp
+      then show ?case using Cons by auto
+    qed
+    with as'(2)
+    show False by simp
+  qed
+qed
+
+lemma idk:
+  assumes "t1 \<rightarrow>\<^sub>t t2" 
+      and "t1 \<rightarrow>\<^sub>t t'"
+    shows "t2 \<rightarrow>\<^sub>t t' \<or> t' \<rightarrow>\<^sub>t t2"
+  using assms
+proof (induction arbitrary: t' rule: reduce.induct)
+  case (refl t)
+  then show ?case by simp
+next
+  case (trans t t2 t3)
+  then consider "t2 \<rightarrow>\<^sub>t t'" | "t' \<rightarrow>\<^sub>t t2" by auto
+  then show ?case 
+  proof (cases)
+    case 1
+    from trans.IH(2)[OF this]
+    show ?thesis .
+  next
+    case 2
+    from reduce.trans[OF this \<open>t2 \<rightarrow>\<^sub>t t3\<close>]
+    show ?thesis by simp
+  qed
+next
+  case (def t1 t2)
+  with nf
+  have "nf_term fi t2" 
+    apply (cases rule: nf_fi.cases)
+    by auto
+  with def
+  show ?case using unique_reduction 
+next
+  case (app as as' f)
+  then show ?case sorry
+qed
+
+lemma nf_reachable: 
+  assumes "t \<rightarrow>\<^sub>t t'" "nf_term fi t'"
+          "t \<rightarrow>\<^sub>t t''"
+    shows "t'' \<rightarrow>\<^sub>t t'"
+  using assms
+proof (induction arbitrary: t'' rule: reduce.induct)
+  case (refl t)
+  then show ?case using nf_cannot_reduce by blast
+next
+  case (trans t1 t2 t3)
+  show ?case
+  proof (cases "t2 = t3")
+    case True
+    then show ?thesis using trans by simp
+  next
+    case False
+    show ?thesis 
+    proof (cases "t'' = t3")
+      case True
+      then show ?thesis using refl by simp
+    next
+      case False
+      show ?thesis 
+      proof (cases "t'' = t2")
+        case True
+        then show ?thesis using trans by simp
+      next
+        case False
+        with \<open>t'' \<noteq> t3\<close> \<open>t2 \<noteq> t3\<close>
+        show ?thesis using trans 
+      qed
+    qed
+  qed
+next
+  case (def t1 t2)
+  then show ?case sorry
+next
+  case (app as as' f)
+  then show ?case sorry
+qed
+
+
 lemma nf_unique:
   assumes 1: "t \<rightarrow>\<^sub>t t'" "nf_term fi t'"
       and 2: "t \<rightarrow>\<^sub>t t''" "nf_term fi t''"
     shows "t' = t''"
   using assms
-proof (induction rule: reduce.induct)
+proof (induction arbitrary: t'' rule: reduce.induct)
   case (refl t)
-  then show ?case sorry
+  then show ?case using nf_cannot_reduce by simp
 next
   case (trans t1 t2 t3)
-  then show ?case sorry
+  from nf_reachable[OF \<open>t1 \<rightarrow>\<^sub>t t''\<close> \<open>nf_term fi t''\<close> \<open>t1 \<rightarrow>\<^sub>t t2\<close>]
+  have "t2 \<rightarrow>\<^sub>t t''" by auto
+  with trans.IH(2) trans.prems
+  show ?case by simp 
 next
   case (def t1 t2)
   then show ?case sorry
